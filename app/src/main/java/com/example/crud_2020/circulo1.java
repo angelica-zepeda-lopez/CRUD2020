5703d3ed1765dd70e82bd90c853012c961e7dee0
angelica-zepeda-lopez <65511625+angelica-zepeda-lopez@users.noreply.github.com>
2020-09-20 23:52:11 -0600

    proyecto crud SQLite2020

---
+++ app/src/main/java/com/example/crud_2020/circulo1.java
@@ -0,0 +1,14 @@
+package com.example.crud_2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class circulo1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.circulo1);
+    }
+}
